0c1ffb22fdc09da61f8ef469fd36582e2eca0f18
nicfb <bowennicf@gmail.com>
2021-12-07 18:24:10 -0600

    day 1

---
+++ 2021/day1.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("day1.txt", "utf-8").split("\n").map(Number);
+
+let prev, totalIncreased = 0;
+input.map((i) => {
+    curr = parseInt(i);
+    if (curr > prev) totalIncreased++;
+    prev = curr;
+});
+
+console.log(totalIncreased);
